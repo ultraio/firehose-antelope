d35bf7c19c37c190d7d13dc6987f82aaf76b9c1c
Frederik Schöll <fschoell@posteo.de>
2023-10-09 13:25:26 +0200

    investigate block marshal error

--- types/block.go
+++ types/block.go
@@ -2,17 +2,21 @@ package types
 
 import (
 	"fmt"
-
 	pbantelope "github.com/pinax-network/firehose-antelope/types/pb/sf/antelope/type/v1"
 	"github.com/streamingfast/bstream"
+	"github.com/streamingfast/logging"
 	pbbstream "github.com/streamingfast/pbgo/sf/bstream/v1"
+	"go.uber.org/zap"
 	"google.golang.org/protobuf/proto"
 )
+
+var zlog, _ = logging.PackageLogger("types", "github.com/streamingfast/firehose-antelope/types")
 
 func BlockFromProto(b *pbantelope.Block) (*bstream.Block, error) {
 
 	content, err := proto.Marshal(b)
 	if err != nil {
+		zlog.Info("failed to marshal to binary", zap.Error(err), zap.Any("block", b))
 		return nil, fmt.Errorf("unable to marshal to binary form: %s", err)
 	}
 
