d97b6c4abe78ee3aef59b14ca0149a39a36fa792
Duncan Dam <59436522+Duncan-Ultra@users.noreply.github.com>
2023-12-07 12:59:29 +0700

    [BLOCK-2035] run local test with docker image (#5)

---
+++ bin/run_local_test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+BIN=fireantelope
+CONFIG=./devel/standard/standard.yaml
+
+rm -rf $ROOT/firehose-data/local-data
+rm -rf $ROOT/firehose-data/storage
+rm -rf $ROOT/firehose-data/app.log.json
+rm -rf $ROOT/firehose-data/reader/data
+rm -rf $ROOT/firehose-data/reader/work
+rm -rf $ROOT/firehose-data/reader/config/protocol_features
+
+cd $ROOT
+if [ -f "$ROOT/fireantelope" ]
+then
+    BIN=./fireantelope
+fi
+
+if [ "$1" == "docker" ]
+then
+    CONFIG=./devel/standard/standard-docker.yaml
+fi
+
+$BIN start -c $CONFIG
